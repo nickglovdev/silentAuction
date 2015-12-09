ad3f8cdaeafd6ba355100fc192c3c2bc7ee5584b
Nicholas Glover <jacksaintjack@gmail.com>
2015-12-09 15:54:35 -0600

    got the wireframe fro createitem made and made some changes to some links on the other pages

--- src/js/components/moderator/createItem.js
+++ src/js/components/moderator/createItem.js
@@ -3,7 +3,24 @@ import React, { PropTypes } from 'react'
 class CreateItem extends React.Component {
   render () {
     return (
-      
+      <section>
+        <h1>Add Items</h1>
+
+        <section className="itemForm">
+          <img src='http://placecage.com/c/200/200'/>
+
+          <h3>Item Name:</h3>
+          <input type='text' placeholder="Item Name"  />
+
+          <h3>Description:</h3>
+          <input type='text' placeholder='Item Description' />
+
+          <h3>Starting Price</h3>
+          <input type='text' placeholder='Starting Price' />
+        </section>
+
+        <button>Submit</button>
+      </section>
     )
   }
 }
