0fd619e6f9fdec64893850d05479130c2ca9a46e
Daniel Opatich <daniel.opatich@gmail.com>
2015-12-14 11:33:26 -0600

    updated localStorage and added logout

--- src/js/components/moderator/dashboard.js
+++ src/js/components/moderator/dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import setup from '../../setup'
+
+import User from '../../models/users'
+
 class Dashboard extends React.Component {
-  render() {
+render() {
     return (
       <div className="dashboard">
         <header>
@@ -19,6 +23,10 @@ class Dashboard extends React.Component {
             <Link to="#">Auction</Link>
             <Link to="#">Auction2</Link>
           </nav>
+          <footer>
+            <button className="navOptions"
+                    onClick={this.logout}>Logout</button>
+          </footer>
         </aside>
         <div className="pageWrap">
           <main>
--- src/js/models/users.js
+++ src/js/models/users.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  jQuery from 'jquery';
+import jQuery from 'jquery';
 
 class User{
   constructor() {
@@ -8,6 +8,27 @@ class User{
     this.token_expires = null;
     this.token_create = null;
     this.listeners = [];
+
+    // deconstructing token information to localStorage that updates the browser
+    // on where the user has navagated. This way you can stay logged in correctly.
+    if (localStorage.getItem('access_token')) {
+      let {
+        access_token,
+        refresh_token,
+        token_expires,
+        token_created
+      } = JSON.parse(localStorage.getItem('access_token'));
+      this.access_token = access_token;
+      this.refresh_token = refresh_token;
+      this.token_expires = expires_in;
+      this.token_created = created_at;
+    }
+  }
+
+  // this function can be refrence in other components to make sure that
+  // user action is only of those who are logged in.
+  isLoggedIn() {
+    return this.access_token !==null;
   }
 
   // Create a function that takes the values given to us from the 'Register',
@@ -23,8 +44,7 @@ class User{
       }
     };
 
-
-    //Shows use if we where able to log in
+    //Shows use if we were able to log in
     jQuery.ajax(options).then(response =>{
       done(null,response);
     }).fail(error => {
@@ -52,12 +72,29 @@ class User{
       this.token_expires = expires_in;
       this.token_created = created_at;
 
+      localStorage.setItem('access_token', JSON.stringify({
+        access_token,
+        refresh_token,
+        expires_in,
+        created_at
+      }));
 
-      //Shows use if we where able to log in and runs through our errors if there are any.
+      //Shows use if we were able to log in and runs through our errors if there are any.
       done(null,response);
     }).fail(error => {
       done(error);
     });
+  };
+
+  // Getting rid of the tracking information provided to the localStorage and
+  // removing access_token so that the user is no longer recognized.
+  logout(data, done) {
+    this.access_token = null;
+    this.refresh_token = null;
+    this.token_expires = null;
+    this.token_created = null;
+
+    localStorage.removeItem('access_token');
   };
 }
 
