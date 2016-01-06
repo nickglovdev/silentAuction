d4974e45cf7712ab09744ce60b19c8cc1f04025b
Daniel Opatich <daniel.opatich@gmail.com>
2016-01-06 17:22:48 -0600

    current

--- src/js/components/moderator/auctionItemsView.js
+++ src/js/components/moderator/auctionItemsView.js
@@ -111,15 +111,16 @@ class AuctionsItemView extends React.Component {
           <Link to={`/auctions/${id}/edit`}>Edit Auction</Link>
             <Link to={`/public/auctions/${id}`}>Public View</Link>
 
+          <h3>Manage Auction</h3>
+
+
             <div className="clipboard">
               <ClipboardButton data-clipboard-text={`http://localhost:8000/#/public/auctions/${id}/items`}>
                 Copy to Clipboard
               </ClipboardButton>
               <input id='publicURL' type="text" value={`http://localhost:8000/#/public/auctions/${id}/items`} readOnly/>
             </div>
-
-          <h3>Manage Auction</h3>
-
+            
           <div className="statusOpen">
             <button className="auctionStatusOpen" onClick={this.handleAuctionOpen}>Open</button>
             <button className="auctionStatusClose" onClick={this.handleAuctionClose}>Close</button>
